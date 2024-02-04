fdd61c074cbd4e3b98c5f99e1c26650cbc2310ac
Rokas <rookas.rudzenskas@gmail.com>
2024-02-04 10:14:39 +0200

    liveblocks functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,11 @@ const workSans = Work_Sans({
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang='en'>
-  <body className={`${workSans.className} bg-primary-grey-200`}>
-  <Room>
-    <TooltipProvider>{children}</TooltipProvider>
-  </Room>
-  </body>
+    <body className={`${workSans.className} bg-primary-grey-200`}>
+      <Room>
+        <TooltipProvider>{children}</TooltipProvider>
+      </Room>
+    </body>
   </html>
 );
 
